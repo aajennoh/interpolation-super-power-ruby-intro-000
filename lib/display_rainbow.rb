faca89775b4d6fce3ba2c472d0d4bb50ac1a0f56
JENNIFER OH <ohjennifer.90@gmail.com>
2019-01-30 14:31:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,8 +4,8 @@ def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
   puts "The first colors in the alphabet is: #{colors[4]}"
   puts "The second colors in the alphabet is: #{colors[3]}"
-  puts "The third colors in the alphabet is: #{letters[5]}"
+  puts "The third colors in the alphabet is: #{colors[5]}"
   puts "The fourth colors in the alphabet is: #{colors[1]}"
   puts "The fifth colors in the alphabet is: #{colors[0]}"
-  puts "The sixth colors in the alphabet is: #{letters[2]}"
+  puts "The sixth colors in the alphabet is: #{colors[2]}"
 end
